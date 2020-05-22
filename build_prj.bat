693709f7ece54b367a81c1796208ea3e5d5a6c19
Oscar Vargas <oscar.reyvarsal@gmail.com>
2020-05-22 22:27:10 +0200

    Update build_prj.bat

--- build_prj.bat
+++ build_prj.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake .. -G"Visual Studio 15 2017 Win64"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" ucloth.sln /target:ALL_BUILD
+cmake .. -G"Visual Studio 16 2019"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe" ucloth.sln /target:ALL_BUILD
